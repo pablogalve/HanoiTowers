e5e9dbab2934889753dfd623f32529b8d2c6f9ac
Pablo <eduinvestor100@gmail.com>
2019-06-02 19:07:13 +0200

    Added 99 levels - up to 101 disks

--- HanoiTowers/main.cpp
+++ HanoiTowers/main.cpp
@@ -9,12 +9,12 @@ int main() {
 	rod rod1;
 	rod rod2;
 	rod rod3;
-
 	rod1.name = "Rod1";
 	rod2.name = "Rod2";
 	rod3.name = "Rod3";
 
-	bool moved;
+	int level = 1; //Disks = level + 2
+
 	bool solved = false; //It indicates if all elements have been moved from origin to destination
 
 	int from, to;
@@ -22,61 +22,78 @@ int main() {
 
 	from = to = 0;
 	from_rod = to_rod = nullptr;
-
-	rod1.addElement(5);
-	rod1.addElement(4);
-	rod1.addElement(3);
-	rod1.addElement(2);
-
-	rod1.printList();
-	rod2.printList();
-	rod3.printList();
-
+		
 	do {
-		cout << "Choose rod '1,2 or 3'." << endl;
-		cout << "Change element from rod: ";
-		cin >> from;
-		cout << "Change element to rod: ";
-		cin >> to;
+		cout << "----- Level " << level << " started! -----" << endl;
+		cout << "----- Good luck! -----" << endl;
 
-		if (from == 1) from_rod = &rod1; //Origin rod
-		else if (from == 2) from_rod = &rod2;
-		else if (from == 3) from_rod = &rod3;
-		else from_rod = 0;
-
-		if (to == 1) to_rod = &rod1;  //Destination rod
-		else if (to == 2) to_rod = &rod2;
-		else if (to == 3) to_rod = &rod3;
-		else to_rod = 0;
-
-		switch (from) {
-		case 1:
-			rod1.moveElement(to_rod);
-			break;
-		case 2:
-			rod2.moveElement(to_rod);
-			break;
-		case 3:
-			rod3.moveElement(to_rod);
-			break;
-		default:
-			cout << "Your 'from' is incorrect." << endl << "Please try again with 1, 2 or 3" << endl;
-			cout << "It will refresh in 5 seconds...";
-			sleep_until(system_clock::now() + seconds(5));
-			break;
+		
+		//We add disks to first row
+		//Disks added = Level + 2
+		for (int i = level+2; i > 0; i--) {
+			rod1.addElement(i);
 		}
-		system("cls");
+
 		rod1.printList();
 		rod2.printList();
 		rod3.printList();
+		do {			
+			cout << "Choose rod '1,2 or 3'." << endl;
+			cout << "FROM: ";
+			cin >> from;
+			cout << "TO: ";
+			cin >> to;
 
-		//It is solved if rods 1 and 2 are empty
-		if (rod1.checkEmpty())
-			if (rod2.checkEmpty())
-				solved = true;
-	} while (!solved);
+			if (from == 1) from_rod = &rod1; //Origin rod
+			else if (from == 2) from_rod = &rod2;
+			else if (from == 3) from_rod = &rod3;
+			else from_rod = 0;
 
-	cout << "Congratulations! " << endl << "You finished the game!" << endl;
+			if (to == 1) to_rod = &rod1;  //Destination rod
+			else if (to == 2) to_rod = &rod2;
+			else if (to == 3) to_rod = &rod3;
+			else to_rod = 0;
+
+			switch (from) {
+			case 1:
+				rod1.moveElement(to_rod);
+				break;
+			case 2:
+				rod2.moveElement(to_rod);
+				break;
+			case 3:
+				rod3.moveElement(to_rod);
+				break;
+			default:
+				cout << "Your 'from' is incorrect." << endl << "Please try again with 1, 2 or 3" << endl;
+				cout << "It will refresh in 5 seconds...";
+				sleep_until(system_clock::now() + seconds(5));
+				break;
+			}
+			system("cls");
+			cout << "LEVEL: " << level << endl;
+			cout << "DISKS: " << level+2 << endl << endl;
+			rod1.printList();
+			rod2.printList();
+			rod3.printList();
+
+			//It is solved if rods 1 and 2 are empty
+			if (rod1.checkEmpty())
+				if (rod2.checkEmpty())
+					solved = true;
+		} while (!solved);
+		system("cls");
+		cout << "Congratulations! " << endl << "You finished level: " << level << endl;
+		level++;
+
+		//We remove all elements before starting next level
+		while (rod1.checkEmpty() == false)
+			rod1.deleteElement();
+		while (rod2.checkEmpty() == false)
+			rod2.deleteElement();
+		while (rod3.checkEmpty() == false)
+			rod3.deleteElement();
+	} while (level < 100);	
 
 	system("pause");
 	return 0;
